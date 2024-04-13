12baae1e8ec6bbe724fdadc5cbcc7281d57aed9d
Ahtisham <ilyasahtsham@gmail.com>
2024-04-13 17:55:25 +0500

    Added the dropdown using Chat GPT, will convert it to custom widget ( check TODO )

--- lib/pages/archive_page.dart
+++ lib/pages/archive_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:todoapp/component/my_app_bar.dart';
+import 'package:todoapp/component/my_drop_down.dart';
 import 'package:todoapp/component/to_do_list.dart';
 import 'package:todoapp/utils/date_formatter.dart';
 
@@ -31,8 +32,6 @@ class _ArchivePageState extends State<ArchivePage> {
     super.initState();
   }
 
-
-
   //delete task
   //delete task
   void deleteTask(int index) {
@@ -50,6 +49,17 @@ class _ArchivePageState extends State<ArchivePage> {
       appBar: const MyAppBar(pageTitle: "Completed Tasks"),
       body: Column(
         children: [
+           Padding(
+            padding: const EdgeInsets.only(left: 25.0, top: 10, right: 25,),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const MyDropdownMenu(),
+                const SizedBox(width: 20,),
+                Icon(Icons.filter_alt, size: 30,),
+              ],
+            ),
+          ),
           Padding(
             padding: const EdgeInsets.only(left: 25.0, top: 10),
             child: Row(
